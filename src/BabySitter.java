4d3a57d3203be9113975a6e345992c88281c7a1f
Lovinder Pnag <lspnag@gmail.com>
2016-03-03 21:01:28 -0500

    first test for valid schedule

--- src/BabySitter.java
+++ src/BabySitter.java
@@ -1,5 +1,15 @@
-/**
- * Created by LoRyder1 on 3/3/16.
- */
+
 public class BabySitter {
+
+    private int startTime;
+    private int endTime;
+
+    public BabySitter(int startTime, int endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public boolean valid() {
+        return false;
+    }
 }
--- test/BabySitterTest.java
+++ test/BabySitterTest.java
@@ -1,5 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BabySitterTest {
 
-    private BabySitter b;
+    private BabySitter sitting;
+
+    @Test
+    public void sittingStartsNoEarlierThanFive() {
+        sitting = new BabySitter(17,28);
+        assertEquals(false, sitting.valid());
+    }
+
+
 }
